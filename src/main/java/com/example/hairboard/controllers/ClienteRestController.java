01552ae0c982c98acf724157765a4628fd787edd
marcserranog <91466710+marcserranog@users.noreply.github.com>
2023-06-01 18:00:45 +0100

    Update RestController añadiendo CrossOrigin uso de CORS en la API REST

--- src/main/java/com/example/hairboard/controllers/ClienteRestController.java
+++ src/main/java/com/example/hairboard/controllers/ClienteRestController.java
@@ -3,12 +3,14 @@ package com.example.hairboard.controllers;
 import com.example.hairboard.models.entity.Cliente;
 import com.example.hairboard.models.services.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
