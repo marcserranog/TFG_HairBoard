929d5b1eea71ad6ccb5a5bf05ede1657282656bb
marcserranog <91466710+marcserranog@users.noreply.github.com>
2023-06-08 10:31:39 +0100

    Clases Entidad Usuario y Role para realizar la seguridad de la web

---
+++ src/main/java/com/example/hairboard/models/dao/IUsuarioDao.java
@@ -0,0 +1,15 @@
+package com.example.hairboard.models.dao;
+
+import com.bolsadeideas.springboot.backend.apirest.models.entity.Usuario;
+import com.example.hairboard.models.entity.Usuario;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IUsuarioDao extends CrudRepository<Usuario, Long>{
+	
+	public Usuario findByUsername(String username);
+	
+	@Query("select u from Usuario u where u.username=?1")
+	public Usuario findByUsername2(String username);
+
+}
