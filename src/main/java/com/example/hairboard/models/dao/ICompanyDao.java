4cb68cbc7ab2261586fb1543d4bf0f16d5f79bba
marcserranog <91466710+marcserranog@users.noreply.github.com>
2023-07-14 16:17:09 +0200

    Delete src directory

--- src/main/java/com/example/hairboard/models/dao/ICompanyDao.java
+++
@@ -1,8 +0,0 @@
-package com.example.hairboard.models.dao;
-
-import com.example.hairboard.models.entity.Company;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ICompanyDao extends JpaRepository<Company, Long> {
-
-}
--- src/main/java/com/example/hairboard/models/services/IUsuarioService.java
+++
@@ -1,8 +0,0 @@
-package com.example.hairboard.models.services;
-
-import com.example.hairboard.models.entity.Usuario;
-
-public interface IUsuarioService {
-
-	public Usuario findByUsername(String username);
-}
